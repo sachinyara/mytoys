b5bc19db6305a7b4401071e12a08b39e1108f6e5
Sachin <sachin.b.sks@gmail.com>
2024-02-19 15:12:39 +0530

    Initial commit of mytoys project.

---
+++ README.md
@@ -0,0 +1,3 @@
+* This project is intended to manage childeren toys. 
+* It helps parents to maintain the inventory ot toys.
+* Keeps records of playtime and how much the kid was engaged.
---
+++ src/main/java/com/app/mytoys/MytoysApplication.java
@@ -0,0 +1,13 @@
+package com.app.mytoys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytoysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytoysApplication.class, args);
+	}
+
+}
